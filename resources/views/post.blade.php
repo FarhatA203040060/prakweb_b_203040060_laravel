41cbb627bddb62badc0f5369b89a1e993196e855
FarhatA22 <farhatabd21@gmail.com>
2022-10-25 23:16:54 +0700

    Menyelesaikan Video ke-8 Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>{{ $post->title }}</h1>
+    <h1>{{ $post->tittle }}</h1>
+
+    <p>By. Farhat Aldjaidi in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
 
     {!! $post->body !!}
 
