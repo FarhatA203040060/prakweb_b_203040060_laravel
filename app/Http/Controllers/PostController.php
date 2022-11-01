aec151281fba83b175a9ab96472b916386f79215
FarhatA22 <farhatabd21@gmail.com>
2022-11-01 22:09:56 +0700

    Menyelesaikan Video ke-13 Searching & Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,27 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $tittle = '';
+
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $tittle = ' in ' . $category->name;
+        }
+
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $tittle = ' by ' . $author->name;
+        }
+
         return view('posts', [
-            "tittle" =>"All Posts",
-            "active" => 'posts',
-            // "posts" => Post::all()
-            "posts" => Post::latest()->get()
+            "tittle" => "All Posts" . $tittle,
+            "active" => "posts",
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString(),
         ]);
     }
-
     public function show(Post $post)
     {
         return view('post', [
             "tittle" => "Single Post",
-            "active" => 'posts',
-            "post" => $post
+            "active" => "posts",
+            "post" => $post,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,24 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "tittle" =>"Home"
+        "tittle" =>"Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('about', function () {
     return view('about', [
-        "tittle" =>"About",
-        "name" => "Farhat Aldjaidi",
-        "email" => "farhataldjaidi@gmail.com",
-        "image" => "profile.jpg"
+        'tittle' =>"About",
+        'active' => 'about',
+        'name' => "Farhat Aldjaidi",
+        'email' => "farhataldjaidi@gmail.com",
+        'image' => "profile.jpg"
     ]);
 });
 
 
 Route::get('blog', [PostController::class, 'index']);
+// Route::get('posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function() {
@@ -44,20 +47,5 @@ Route::get('/categories', function() {
         'tittle' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('posts', [
-        'tittle' => "Post By Category :$category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'tittle' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
